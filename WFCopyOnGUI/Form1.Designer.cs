3b453579f07852fce04d68db62418ad5144d112c
Mujo Didic <80248965+MujoDidic@users.noreply.github.com>
2023-05-22 20:54:13 +0200

    Projektdateien hinzufügen.

---
+++ WFCopyOnGUI/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace WFCopyOnGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.sourceLbl = new System.Windows.Forms.Label();
+            this.endPointLbl = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(12, 126);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(197, 126);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // sourceLbl
+            // 
+            this.sourceLbl.AutoSize = true;
+            this.sourceLbl.Location = new System.Drawing.Point(12, 9);
+            this.sourceLbl.Name = "sourceLbl";
+            this.sourceLbl.Size = new System.Drawing.Size(41, 13);
+            this.sourceLbl.TabIndex = 2;
+            this.sourceLbl.Text = "Source";
+            // 
+            // endPointLbl
+            // 
+            this.endPointLbl.AutoSize = true;
+            this.endPointLbl.Location = new System.Drawing.Point(219, 9);
+            this.endPointLbl.Name = "endPointLbl";
+            this.endPointLbl.Size = new System.Drawing.Size(53, 13);
+            this.endPointLbl.TabIndex = 3;
+            this.endPointLbl.Text = "End Point";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Item 1",
+            "Item 2",
+            "Item 3 ",
+            "Item 4",
+            "Item 5"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "List Item1",
+            "List Item 2",
+            "List Item 3 ",
+            "List Item 4",
+            "List Item 5"});
+            this.listBox1.Location = new System.Drawing.Point(164, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(108, 95);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.endPointLbl);
+            this.Controls.Add(this.sourceLbl);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCopy);
+            this.Name = "Form1";
+            this.Text = "CopyOnGUI";
+            this.Load += new System.EventHandler(this.btnCopy_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label sourceLbl;
+        private System.Windows.Forms.Label endPointLbl;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WFCopyOnGUI/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFCopyOnGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
+            
+            string tempText = endPointLbl.Text;
+
+            endPointLbl.Text = sourceLbl.Text;
+
+            sourceLbl.Text = tempText;
+
+            //Copy from ComboBox
+
+            sourceLbl.Text = comboBox1.SelectedItem.ToString();
+
+            //Copy from End Point to List box
+
+
+            listBox1.Items.Add(endPointLbl.Text);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
